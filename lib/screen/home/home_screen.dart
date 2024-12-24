08db0346a263e43a90f469f7f2af15934deee544
Haruhiko Takada <haruhiko.takada@gmail.com>
2024-12-24 09:58:07 +0900

    プログレス表示

--- lib/screen/home/home_screen.dart
+++ lib/screen/home/home_screen.dart
@@ -21,50 +21,55 @@ class HomeScreen extends HookConsumerWidget {
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: const Text("Landmarks"),
       ),
-      body: Column(
-        children: [
-          SwitchListTile(
-            title: const Text('Favorites only'),
-            value: uiModel.favoritesOnly,
-            onChanged: (value) {
-              eventHandler.onFavoritesOnlyChanged(value);
-            },
-          ),
-          const Divider(
-            height: 1,
-          ),
-          Expanded(
-            child: ListView.separated(
-              itemCount: uiModel.landmarks.length,
-              separatorBuilder: (context, index) => const Divider(
-                height: 1,
-              ), // 区切り線を追加
-              itemBuilder: (context, index) {
-                final landmark = uiModel.landmarks[index];
-                return Container(
-                  margin: const EdgeInsets.all(16),
-                  child: Row(
-                    children: [
-                      Image.asset(landmark.imageUrl,
-                          width: 64, height: 64, fit: BoxFit.cover),
-                      const SizedBox(width: 16),
-                      Expanded(
-                        child: Text(
-                          landmark.name,
-                          style: Theme.of(context).textTheme.bodyLarge,
+      body: uiModel.progress // ここで条件分岐
+          ? const Center(
+              // プログレスインジケーターを中央に表示
+              child: CircularProgressIndicator(),
+            )
+          : Column(
+              children: [
+                SwitchListTile(
+                  title: const Text('Favorites only'),
+                  value: uiModel.favoritesOnly,
+                  onChanged: (value) {
+                    eventHandler.onFavoritesOnlyChanged(value);
+                  },
+                ),
+                const Divider(
+                  height: 1,
+                ),
+                Expanded(
+                  child: ListView.separated(
+                    itemCount: uiModel.landmarks.length,
+                    separatorBuilder: (context, index) => const Divider(
+                      height: 1,
+                    ), // 区切り線を追加
+                    itemBuilder: (context, index) {
+                      final landmark = uiModel.landmarks[index];
+                      return Container(
+                        margin: const EdgeInsets.all(16),
+                        child: Row(
+                          children: [
+                            Image.asset(landmark.imageUrl,
+                                width: 64, height: 64, fit: BoxFit.cover),
+                            const SizedBox(width: 16),
+                            Expanded(
+                              child: Text(
+                                landmark.name,
+                                style: Theme.of(context).textTheme.bodyLarge,
+                              ),
+                            ),
+                            if (landmark.isFavorite)
+                              Icon(Icons.star,
+                                  color: Theme.of(context).colorScheme.primary),
+                          ],
                         ),
-                      ),
-                      if (landmark.isFavorite)
-                        Icon(Icons.star,
-                            color: Theme.of(context).colorScheme.primary),
-                    ],
+                      );
+                    },
                   ),
-                );
-              },
+                ),
+              ],
             ),
-          ),
-        ],
-      ),
     );
   }
 }
