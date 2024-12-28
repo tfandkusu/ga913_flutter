b90891086e9d852ecf11d088bdb4c440be3e2c60
Haruhiko Takada <haruhiko.takada@gmail.com>
2024-12-28 23:14:05 +0900

    lint エラー修正

--- lib/screen/home/home_event_handler.dart
+++ lib/screen/home/home_event_handler.dart
@@ -1,8 +1,6 @@
-import 'package:auto_route/auto_route.dart';
 import 'package:flutter/widgets.dart';
 import 'package:ga913_flutter/app_router.dart';
 import 'package:ga913_flutter/data/repository/landmark_repository.dart';
-import 'package:ga913_flutter/model/landmark.dart';
 import 'package:ga913_flutter/screen/home/home_ui_state_notifier.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
