ded702e3f41021744c60fdc6a597db6128fe68c3
Haruhiko Takada <haruhiko.takada@gmail.com>
2024-12-31 01:18:42 +0900

    詳細画面を設置

--- lib/screen/detail/detail_screen.dart
+++ lib/screen/detail/detail_screen.dart
@@ -1,18 +1,26 @@
+// ignore_for_file: unused_local_variable
+
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
+import 'package:ga913_flutter/screen/detail/detail_event_handler.dart';
+import 'package:ga913_flutter/screen/detail/detail_ui_model_provider.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 @RoutePage()
 class DetailScreen extends HookConsumerWidget {
-  // ignore: unused_field
   final int _landmarkId;
-
   const DetailScreen(this._landmarkId, {super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final uiModel = ref.watch(detailUiModelProvider(_landmarkId));
+    final eventHandler = ref.read(detailEventHandlerProvider(_landmarkId));
+
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        title: const Text('Detail'),
+      ),
+      body: Container(),
     );
   }
 }
