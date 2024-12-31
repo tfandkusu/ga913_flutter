5540a8e6267a23e1c48a39aed0b7f6cad8a0d6ff
Haruhiko Takada <haruhiko.takada@gmail.com>
2024-12-31 10:43:33 +0900

    詳細画面を設置する

--- lib/screen/detail/detail_screen.dart
+++ lib/screen/detail/detail_screen.dart
@@ -15,12 +15,47 @@ class DetailScreen extends HookConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     final uiModel = ref.watch(detailUiModelProvider(_landmarkId));
     final eventHandler = ref.read(detailEventHandlerProvider(_landmarkId));
-
+    final landmark = uiModel.landmark;
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Detail'),
+        title: Text(landmark?.name ?? ''),
       ),
-      body: Container(),
+      body: landmark != null
+          ? ListView(
+              children: [
+                Center(
+                  child: Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: _image(landmark.imageUrl),
+                  ),
+                ),
+              ],
+            )
+          : Container(),
+    );
+  }
+
+  Widget _image(String imageUrl) {
+    return Material(
+      elevation: 8,
+      shape: const CircleBorder(),
+      child: Container(
+        decoration: BoxDecoration(
+          shape: BoxShape.circle,
+          border: Border.all(
+            color: Colors.white,
+            width: 4,
+          ),
+        ),
+        child: ClipOval(
+          child: Image.asset(
+            imageUrl,
+            width: 250,
+            height: 250,
+            fit: BoxFit.cover,
+          ),
+        ),
+      ),
     );
   }
 }
